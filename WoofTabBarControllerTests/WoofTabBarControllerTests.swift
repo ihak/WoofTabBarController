792185e2979eef117cf0fd606c0a2cd6fed87a2b
Hassan Ahmed Khan <hassan.ahmed@arpatech.com>
2019-11-22 12:18:17 +0500

    Xcode project setup

---
+++ WoofTabBarControllerTests/WoofTabBarControllerTests.swift
@@ -0,0 +1,34 @@
+//
+//  WoofTabBarControllerTests.swift
+//  WoofTabBarControllerTests
+//
+//  Created by Hassan Ahmed on 22/11/2019.
+//  Copyright © 2019 Arpatech Pvt. Ltd. All rights reserved.
+//
+
+import XCTest
+@testable import WoofTabBarController
+
+class WoofTabBarControllerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
