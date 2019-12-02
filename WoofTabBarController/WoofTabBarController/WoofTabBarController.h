2b63b5bec43a91de73e857f77c25cb4c57ba944b
Hassan Ahmed Khan <hassan.ahmed@arpatech.com>
2019-12-02 09:54:19 +0500

    Added podspec and initiated the framework project

---
+++ WoofTabBarController/WoofTabBarController/WoofTabBarController.h
@@ -0,0 +1,19 @@
+//
+//  WoofTabBarController.h
+//  WoofTabBarController
+//
+//  Created by Hassan Ahmed on 29/11/2019.
+//  Copyright © 2019 Arpatech Pvt. Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WoofTabBarController.
+FOUNDATION_EXPORT double WoofTabBarControllerVersionNumber;
+
+//! Project version string for WoofTabBarController.
+FOUNDATION_EXPORT const unsigned char WoofTabBarControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WoofTabBarController/PublicHeader.h>
+
+
